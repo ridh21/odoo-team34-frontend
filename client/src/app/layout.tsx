04296ff0c3ec7d31457ca20e0ff13acee5df30e0
ridh21 <ridhampatel2k4@gmail.com>
2025-03-22 10:57:16 +0530

    nextjs with shadcn setup done

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Instrument_Sans, Roboto_Flex } from "next/font/google";
+import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
+
+
+const instrumentSans = Instrument_Sans({
+  variable: "--font-instrument-sans",
+  subsets: ["latin"],
+});
+
+const robotoFlex = Roboto_Flex({
+  variable: "--font-roboto-flex",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "KrushiMart",
+  description: "Bridging the gap between natural farmers and concious customers",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" {...{ webcrx: "false" }}>
+      <head>
+      </head>
+      <body
+        className={`${instrumentSans.variable} ${robotoFlex.variable} bg-[#FAFAFA] antialiased`}
+      >
+        <NextTopLoader height={3} color="#1565C0" showSpinner={false} />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ client/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Button> This is the demo button</Button>
+    </div>
+  )
+}
+
+export default page
